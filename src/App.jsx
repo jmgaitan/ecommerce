c4460dd11cdc587230e73dc651eb67746205fec8
JuanMa.Gaitan <jmgaitan93@gmail.com>
2024-05-27 17:48:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { products } from './temp/products.json'
+import { Products } from './components/Products.jsx'
+
+
+function App() {
+  return (
+    <>
+      <Products products={products} />
+    
+    </>
+  )
+}
+
+export default App
